1461c46fe989b94e0fe3d193f459217dc3d13712
lwkin <lwkin2021@gmail.com>
2024-12-12 23:40:07 +0800

    Site updated: 2024-12-12 23:40:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/使用安知鱼主题进行个性化配置，涵盖…/","undefined/hello-world/"];function toRandomPost(){
+var posts=["lohas/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
